e4467f6abff7107d0f96d557eba46a86f50a3b85
Naim Imran <naim.nrb@gmail.com>
2022-11-08 01:54:55 -0500

    project designed

---
+++ src/main/java/automation/selenium4Framework/App.java
@@ -0,0 +1,13 @@
+package automation.selenium4Framework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
