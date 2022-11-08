1bc328a82e211dfec7aece780799ddad986aa830
Naim Imran <naim.nrb@gmail.com>
2022-11-08 04:11:30 -0500

    code added in standAloneTest class

--- src/test/java/automation/selenium4Framework/StandaloneTest.java
+++ src/test/java/automation/selenium4Framework/StandaloneTest.java
@@ -6,12 +6,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+import java.time.Duration;
+
 public class StandaloneTest {
 
 	public static void main(String[] args) {
 
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://rahulshettyacademy.com/client");
 		driver.findElement(By.id("userEmail")).sendKeys("naayeem@gmail.com");
 		driver.findElement(By.id("userPassword")).sendKeys("Abcd1234$");
