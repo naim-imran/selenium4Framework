da833eef380d0e0551ddb53c550104ff5fac736f
Naim Imran <naim.nrb@gmail.com>
2022-11-09 04:00:27 -0500

    code added for My Cart page

--- src/test/java/automation/selenium4Framework/StandaloneTest.java
+++ src/test/java/automation/selenium4Framework/StandaloneTest.java
@@ -2,15 +2,22 @@ package automation.selenium4Framework;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 import java.time.Duration;
+import java.util.List;
 
 public class StandaloneTest {
 
 	public static void main(String[] args) {
+
+		String expectedProductName = "zara coat 3";
 
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
@@ -20,7 +27,29 @@ public class StandaloneTest {
 		driver.findElement(By.id("userEmail")).sendKeys("naayeem@gmail.com");
 		driver.findElement(By.id("userPassword")).sendKeys("Abcd1234$");
 		driver.findElement(By.id("login")).click();
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.mb-3")));
+		List<WebElement> products = driver.findElements(By.cssSelector("div.mb-3"));
+
+		WebElement expectedProduct = products.stream()
+				.filter(p -> p.findElement(By.cssSelector("b")).getText().equalsIgnoreCase(expectedProductName))
+				.findFirst().get();
+		expectedProduct.findElement(By.cssSelector(".card-body button:last-of-type")).click();
+
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#toast-container")));
+		wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector(".ng-animating"))));
+
+		driver.findElement(By.cssSelector("[routerlink*='cart']")).click();
+
+		List<WebElement> productsInCart = driver.findElements(By.cssSelector(".cartSection h3"));
+		Boolean match = productsInCart.stream()
+				.anyMatch(q -> q.getText().equalsIgnoreCase(expectedProductName));
+		System.out.println(match);
+		Assert.assertTrue(match);
 		
+
 	}
 
 }
