b96959116e2853fc2b21f9a87bd3ddaf818f4119
naayeemVM <naim.nrb@gmail.com>
2023-03-14 02:56:17 -0400

    SetupBrowser class modified

--- src/test/java/testListeners/SetupBrowser.java
+++ src/test/java/testListeners/SetupBrowser.java
@@ -37,8 +37,10 @@ public class SetupBrowser {
 		String browserName = properties.getProperty("browser");
 
 		if (browserName.equalsIgnoreCase("chrome")) {
+			
 			ChromeOptions co = new ChromeOptions();
 			co.addArguments("--remote-allow-origins=*");
+			
 			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver(co);
 		} else if (browserName.equalsIgnoreCase("firefox")) {
